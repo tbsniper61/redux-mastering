924ba5cae4f2fdf30d9a81cfb1fc56bc3ae117ed
tbsniper61 <tbsniper61@gmail.com>
2017-09-18 21:36:43 -0700

    god now prints state for stateful components

--- src/components/God.js
+++ src/components/God.js
@@ -2,24 +2,42 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import chatReducer from '../reducers/chat-reducer';
 import ride from 'ride';
+import Tree from './Tree';
+import D3Tree from './D3Tree'
+import Page from './Page';
+// import Tree from 'react-d3-tree';
+// import TreeD3 from './treeD3';
 
 class God extends Component {
-  state = { tree: {} }
+  state = {
+    toggle: false,
+  }
 
   traverseGOD = () => {
+    console.log(this)
     let components = [];
     let dom = this._reactInternalInstance._renderedComponent
 
     recursiveTraverse(dom, components)
-    console.log('return')
     console.log(components)
+    const sendData = { type: "FROM_PAGE", text: 'hello from postMessage' }
     return components
+
 
     function recursiveTraverse(component, parentArr) {
       let newComponent = {}
       if (!component._currentElement) return
       newComponent.name = component._currentElement.type.name || component._currentElement.type
       newComponent.children = [];
+
+      // track state
+      if (component) {
+        if (component._instance) {
+          if (component._instance.state) {
+            console.log('STATE !!!!!', component._instance.state)
+          }    
+        }
+      }
 
       const componentChildren = component._renderedChildren
       parentArr.push(newComponent);
@@ -35,20 +53,26 @@ class God extends Component {
   }
 
   componentDidMount() {
-    ride(React.Component.prototype, 'setState')
-      .after(() => {
-        this.traverseGOD()
-      });
-    console.log(this)
+    this.traverseGOD()
+    this.setState({
+      toggle: true,
+    })
   }
-
   componentDidUpdate() {
     this.traverseGOD()
+
+    ride(React.Component.prototype, 'setState')
+      .after(() => {
+        this.traverseGOD();
+      })
   }
 
   render() {
     return (
       <div>
+        {this.state.toggle &&
+          <D3Tree traverse={this.traverseGOD} />
+        }
         {this.props.children}
       </div>
     )
